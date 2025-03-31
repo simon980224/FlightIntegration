b213ba31eebfe20770d465410adce4c959367d22
allen91917 <zhangyuan91917@gmail.com>
2025-03-31 19:55:19 +0800

    feat:新增airline&airport

--- app.py
+++ app.py
@@ -1,51 +1,59 @@
-from flask import Flask, app, render_template
+from flask import Flask, render_template, request, jsonify
 import pyodbc
 
 app = Flask(__name__)
 
 # MSSQL 資料庫連線設定
 conn_str = (
-    "DRIVER={ODBC Driver 17 for SQL Server};"  # 根據你安裝的版本，可能是 18
-    "SERVER=140.131.114.241;"                        # 改成你的 SQL Server 名稱或 IP
-    "DATABASE=114-FlightIntegration_DB;"                   # 改成你的資料庫名稱
-    "UID=adminfid;"                           # 改成你的帳號
-    "PWD=Flight_admin123@;"                       # 改成你的密碼
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=140.131.114.241;"
+    "DATABASE=114-FlightIntegration_DB;"
+    "UID=adminfid;"
+    "PWD=Flight_admin123@;"
 )
 
-# 設定連線參數
-server = '140.131.114.241'
-database = '114-FlightIntegration_DB'
-username = 'adminfid'
-password = 'Flight_admin123@'
-    
-    # 建立連線字串
-connection_string = (
-        'DRIVER={ODBC Driver 17 for SQL Server};'
-        f'SERVER={server};'
-        f'DATABASE={database};'
-        f'UID={username};'
-        f'PWD={password}'
-    )
-
-# 撈取資料
+# 撈取 Flight 資料
 def get_data():
-    
-        conn = pyodbc.connect(conn_str)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * from Flight")  # 改成你的資料表名稱
-        columns = [column[0] for column in cursor.description]
-        rows = cursor.fetchall()
-        data = [dict(zip(columns, row)) for row in rows]
-        cursor.close()
-        conn.close()
-        return data
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Flight")
+    columns = [column[0] for column in cursor.description]
+    rows = cursor.fetchall()
+    data = [dict(zip(columns, row)) for row in rows]
+    cursor.close()
+    conn.close()
+    return data
 
+# 撈取 Airport 資料
+def get_airport_data():
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Airport")
+    columns = [column[0] for column in cursor.description]
+    rows = cursor.fetchall()
+    data = [dict(zip(columns, row)) for row in rows]
+    cursor.close()
+    conn.close()
+    return data
 
+def get_airline_data():
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Airline")
+    columns = [column[0] for column in cursor.description]
+    rows = cursor.fetchall()
+    data = [dict(zip(columns, row)) for row in rows]
+    cursor.close()
+    conn.close()
+    return data
 
+# 頁面 index：同時顯示兩組資料
 @app.route('/')
 def index():
-    data = get_data()
-    return render_template('index.html', title='首頁', data=data)
+    flight_data = get_data()
+    airport_data = get_airport_data()
+    airline_data = get_airline_data()
+    return render_template('index.html', title='首頁', flight_data=flight_data, airport_data=airport_data, airline_data=airline_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
