e343f90f8aa74aa3f63982aca50c0b3ed165f2ab
Zheniiii0918 <kate920915@gmail.com>
2025-07-13 12:24:05 +0800

    feat: 新增 Toast 提示功能，重構登入及註冊邏輯，移除不必要的程式碼

--- static/script.js
+++ static/script.js
@@ -1,3 +1,15 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+    }
+});
+
 function showLoading(title, text) {
     Swal.fire({
         title: title,
@@ -14,7 +26,7 @@ function closeLoading() {
 }
 
 function showToast(title, text, icon = 'success') {
-    Toast.fire({
+    return Toast.fire({
       icon,    // success / error / warning / info / question
       title,
       text     // 可不傳，用於補充說明
