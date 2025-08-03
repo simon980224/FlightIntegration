216617e9e44cef78bcb94a3cf04a6420e4b8aa3d
Zheniiii0918 <kate920915@gmail.com>
2025-08-03 14:57:51 +0800

    feat: 在票券頁面中新增使用者驗證，顯示使用者資料、航班資訊及票券資訊，並處理資料獲取錯誤提示

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from service import search_service,user_service
 from functools import wraps
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'your-development-secret-key'
@@ -168,8 +168,75 @@ def process_booking(flight_id):
     })
 
 @app.route('/ticket')
+@login_required
 def ticket():
-    return render_template('ticket.html')
+    user_id = session.get('user_id')
+    if not user_id:
+        flash('請先登入', 'error')
+        return redirect(url_for('login'))
+
+    # user_data_result = user_service.GetUserData(user_id)
+    user_data_result = {
+        "success": True,
+        "data": {
+            "username": "Admin",
+            "email": "12345@example.com",
+            "gender": "男性",
+            "birth_date": "1990-01-01",
+            "nationality": "中國",
+            "passport_number": "A123456789"
+        }
+    }
+
+    if user_data_result["success"]:
+        user = user_data_result["data"]
+    else:
+        flash('無法獲取使用者資料', 'error')
+        user = {} # or handle error appropriately
+    
+    time = (datetime.strptime('2025-07-13 10:00', '%Y-%m-%d %H:%M') -
+            datetime.strptime('2025-07-13 08:00', '%Y-%m-%d %H:%M')).total_seconds() / 3600
+
+    flight_data_result = {
+        "success": True,
+        "data": {
+            "flight_id": "EVA_20250713_B7502_TSA_PVG",
+            "flight_no": "B7502",
+            "airline_id": "BR",
+            "airline_name": "長榮航空",
+            "d_airport_id": "TSA",
+            "d_airport_name": "桃園國際機場",
+            "a_airport_id": "PVG",
+            "a_airport_name": "上海浦東國際機場",
+            "d_time": "2025-07-13 08:00",
+            "a_time": "2025-07-13 10:00",
+            "flight_time": f"{time:.1f}"
+        }
+    }
+
+    if flight_data_result["success"]:
+        flight = flight_data_result["data"]
+    else:
+        flash('無法獲取航班資料', 'error')
+        flight = {} # or handle error appropriately
+
+    ticket_data_result = {
+        "success": True,
+        "data": {
+            "ticket_id": "EVA_20250713_B7502_TSA_PVG",
+            "seat_id": "A1",
+            "price": "13500",
+        }
+    }
+
+    if ticket_data_result["success"]:
+        ticket = ticket_data_result["data"]
+    else:
+        flash('無法獲取票券資料', 'error')
+        ticket = {} # or handle error appropriately
+
+    return render_template('ticket.html', user=user, flight=flight, ticket=ticket)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
